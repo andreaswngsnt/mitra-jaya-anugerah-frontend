2d6382076a94ce8404af0ae30426fd5bba5c55d6
Andreas Wangsanata <40526361+andreaswngsnt@users.noreply.github.com>
2019-11-20 19:03:06 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, {Html, Head, Main, NextScript} from "next/document"
-import {fontFamily} from "../vars/styles"
+import {fontFamily} from "../styles/fonts"
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx) {
